b87fc5537639ea57534daf2c24356e9fb2aa7c1f
Pratham khatri <khatripratham349@gmail.com>
2024-09-20 01:46:36 +0530

    fggg

---
+++ queuee/reverseQueue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void resverseQueue(queue<int>&q){
+    stack<int> s;
+    while(!q.empty()){
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+}
