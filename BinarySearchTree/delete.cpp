8545eff90f8bd3a35e4d9ee6a16c7c8cd80edd32
Pratham khatri <khatripratham349@gmail.com>
2024-09-24 18:52:19 +0530

    changessssss

--- BinarySearchTree/delete.cpp
+++ BinarySearchTree/delete.cpp
@@ -1,3 +1,57 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+struct Node
+{
+    int key;
+    Node *left, *right;
+    Node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+Node *getSucc(Node *root)
+{
+    Node *curr = roo->right;
+    while (curr != NULL && curr->left != NULL)
+    {
+        curr = curr->left;
+    }
+    return curr;
+}
+
+Node *delNode(Node *root, int x)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    else if (root->left == NULL)
+    {
+        Node *temp = root->right;
+        delete root;
+        return temp;
+    }
+    else if (root->right == NULL)
+    {
+        Node *temp = root->left;
+        delete root;
+        return temp;
+    }
+    else if (root->key > x)
+    {
+        root->left = delNode(root->left, x);
+    }
+    else if (root->key < x)
+    {
+        root->right = delNode(root->right, x);
+    }
+    else
+    {
+        Node *succ = getSucc(root);
+        root->key = succ->key;
+        root->right = delNode(root->right, succ->key);
+    }
+}
--- BinarySearchTree/insert.cpp
+++ BinarySearchTree/insert.cpp
@@ -30,6 +30,9 @@ struct Node
     }
 }
 
+
+//iterative approach
+
 void insert(Node *root, int x)
 {
     Node *temp = new Node(x);
@@ -63,4 +66,20 @@ void insert(Node *root, int x)
     {
         parent->right = temp;
     }
+}
+
+
+//recursive approach
+Node* insert(Node*root, int x){
+    if(root==NULL){
+        return new Node(x);
+    }
+    else if(root->key<x){
+        root->right=insert(root->right,x);
+    }
+    else if(root->key>x){
+        root->left=insert(root->left,x);
+    }
+
+    return root;
 }
